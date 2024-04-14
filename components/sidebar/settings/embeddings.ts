26696cae7cadbe445d60d10130d9e0a668425025
SamIron0 <samuelironkwec@gmail.com>
2024-04-14 17:45:19 -0500

    x

--- components/sidebar/settings/embeddings.ts
+++ components/sidebar/settings/embeddings.ts
@@ -8,33 +8,9 @@ interface EmbeddingsProps {
   body: string
 }
 
-export const insertEmbeddings: FC<EmbeddingsProps> = async () => {
-  const generateEmbedding = await pipeline(
-    "feature-extraction",
-    "Supabase/gte-small"
-  )
-
-  const title = "First post!"
-  const body = "Hello world!"
-
-  // Generate a vector using Transformers.js
-  const output = await generateEmbedding(body, {
-    pooling: "mean",
-    normalize: true
-  })
-
-  // Extract the embedding output
-  const embedding = Array.from(output.data)
-
-  // Store the vector in Postgres
-  const { data, error } = await supabase.from("documents").insert({
-    title,
-    body,
-    embedding
-  })
-  return data
-}
 export const updateEmbeddings: FC<EmbeddingsProps> = async ({
+  title,
+  body,
   workspace_id
 }: EmbeddingsProps) => {
   const generateEmbedding = await pipeline(
@@ -42,8 +18,8 @@ export const updateEmbeddings: FC<EmbeddingsProps> = async ({
     "Supabase/gte-small"
   )
 
-  const title = "First post!"
-  const body = "Hello world!"
+  //const title = "First post!"
+  //const body = "Hello world!"
 
   // Generate a vector using Transformers.js
   const output = await generateEmbedding(body, {
