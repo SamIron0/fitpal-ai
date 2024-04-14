eba36f98cff615ece709c43a96a563d77c4b3dad
SamIron0 <samuelironkwec@gmail.com>
2024-04-14 06:52:18 -0500

    added sliders

--- components/sidebar/settings/macros.tsx
+++ components/sidebar/settings/macros.tsx
@@ -1,11 +1,47 @@
 import { Slider } from "../../ui/slider"
 
-export const Macros = ({}) => {
+interface MacrosProps {
+  protein: number
+  setProtein: Function
+  carbs: number
+  setCarbs: Function
+  fat: number
+  setFat: Function
+}
+export const Macros = ({
+  protein,
+  setProtein,
+  carbs,
+  setCarbs,
+  fat,
+  setFat
+}: MacrosProps) => {
   return (
     <>
       <div className="mt-6 space-y-3">
         <div className="flex items-center space-x-1">
-          <div>Protein:</div>
+          <div className="text-muted-foreground mb-1 mt-2 text-sm font-semibold">
+            Protein
+          </div>
+
+          <div></div>
+        </div>
+
+        <Slider
+          value={[protein]}
+          onValueChange={values => {
+            setProtein(values[0])
+          }}
+          min={20}
+          max={500}
+          step={5}
+        />
+      </div>
+      <div className="mt-6 space-y-3">
+        <div className="flex items-center space-x-1">
+          <div className="text-muted-foreground mb-1 mt-2 text-sm font-semibold">
+            Carbs
+          </div>
 
           <div></div>
         </div>
@@ -18,11 +54,22 @@ export const Macros = ({}) => {
           step={5}
         />
       </div>
-      <div className="text-muted-foreground font-bolds mb-1 mt-2 text-sm">
-        Carbs
-      </div>
-      <div className="text-muted-foreground font-bolds mb-1 mt-2 text-sm">
-        Fat
+      <div className="mt-6 space-y-3">
+        <div className="flex items-center space-x-1">
+          <div className="text-muted-foreground mb-1 mt-2 text-sm font-semibold">
+            Fats
+          </div>
+
+          <div></div>
+        </div>
+
+        <Slider
+          value={[]}
+          onValueChange={() => {}}
+          min={20}
+          max={500}
+          step={5}
+        />
       </div>
     </>
   )
--- components/sidebar/settings/settings.tsx
+++ components/sidebar/settings/settings.tsx
@@ -7,20 +7,33 @@ import { DietProvider } from "@/types"
 interface SettingsProps {}
 export const Settings: FC<SettingsProps> = () => {
   const [selectedDiet, setSelectedDiet] = useState<DietProvider>("none")
+  const [protein, setProtein] = useState(0)
+  const [carbs, setCarbs] = useState(0)
+  const [fat, setFat] = useState(0)
+  const [calories, setCalories] = useState(0)
   const updateSettings = (selectedDiet: DietProvider) => {
     console.log(selectedDiet)
   }
   return (
-    <>
+    <div className="">
+      <div className="text-muted-foreground mb-1 text-sm font-semibold">
+        Diet
+      </div>
+      <DietSelect onSelect={setSelectedDiet} selectedDiet={selectedDiet} />
+      <Macros
+        protein={protein}
+        setProtein={setProtein}
+        carbs={carbs}
+        setCarbs={setCarbs}
+        fat={fat}
+        setFat={setFat}
+      />
       <Button
         className="mb-3 flex  h-[36px] grow"
         onClick={() => updateSettings(selectedDiet)}
       >
         Save{" "}
       </Button>
-      <div className="text-muted-foreground font-bolds mb-1 text-sm">Diet</div>
-      <DietSelect onSelect={setSelectedDiet} selectedDiet={selectedDiet} />
-      <Macros />
-    </>
+    </div>
   )
 }
