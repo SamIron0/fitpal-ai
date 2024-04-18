3ae8115c3ad1a0eeae038e61902e54ce730d3409
SamIron0 <samuelironkwec@gmail.com>
2024-04-18 17:11:47 -0500

    x

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
   const { theme } = useTheme()
 
   return (
-    <div className="flex w-full flex-row justify-center">
+    <div className="flex w-full flex-col justify-center">
       <Navbar />
 
       <div className="mx-auto max-w-4xl px-4 sm:px-6">
