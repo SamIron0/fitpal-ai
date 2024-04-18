c48a9e0d5cb7c6527d8a63be4692300d521b6712
SamIron0 <samuelironkwec@gmail.com>
2024-04-17 21:26:41 -0500

    x

--- components/sidebar/settings/macros.tsx
+++ components/sidebar/settings/macros.tsx
@@ -35,9 +35,9 @@ export const Macros: FC<MacrosProps> = ({
     Math.round(((fat * 9) / calories) * 100)
   )
   useEffect(() => {
-    setProtein((percentProtein * 0.01 * calories) / 4)
-    setCarbs((percentCarbs * 0.01 * calories) / 4)
-    setFat((percentFat * 0.01 * calories) / 9)
+    setProtein(Math.round((percentProtein * 0.01 * calories) / 4))
+    setCarbs(Math.round((percentCarbs * 0.01 * calories) / 4))
+    setFat(Math.round((percentFat * 0.01 * calories) / 9))
   }, [calories])
   const onChangeFat = (value: number) => {
     setPercentFat(value)
