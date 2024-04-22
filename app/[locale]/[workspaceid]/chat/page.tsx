07fe59f0f50d6a1a585fce4ebad552f1591f2af7
SamIron0 <samuelironkwec@gmail.com>
2024-04-22 16:26:12 -0500

    x

--- app/[locale]/[workspaceid]/chat/page.tsx
+++ app/[locale]/[workspaceid]/chat/page.tsx
@@ -14,12 +14,10 @@ export default function ChatPage() {
   return (
     <>
       {chatMessages.length === 0 ? (
-        <div className="relative flex h-full flex-col items-center justify-center">
-          <div className="top-50% left-50% -translate-x-50% -translate-y-50% absolute mb-20">
+        <div className="relative mt-12 flex h-full flex-col items-center">
+          <div className="top-50% left-50% -translate-x-50% -translate-y-50%  mb-12">
             <Brand theme={theme === "dark" ? "dark" : "light"} />
           </div>
-
-          <div className="flex grow flex-col items-center justify-center" />
 
           <div className="w-full min-w-[300px] items-end px-2 pb-3 pt-0 sm:w-[600px] sm:pb-8 sm:pt-5 md:w-[700px] lg:w-[700px] xl:w-[800px]">
             <ChatInput />
