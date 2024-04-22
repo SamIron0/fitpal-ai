7c739d073e6fe3cb063c5d1d72f85727657253d8
SamIron0 <samuelironkwec@gmail.com>
2024-04-22 16:51:41 -0500

    x

--- app/[locale]/[workspaceid]/chat/page.tsx
+++ app/[locale]/[workspaceid]/chat/page.tsx
@@ -14,7 +14,7 @@ export default function ChatPage() {
   return (
     <>
       {chatMessages.length === 0 ? (
-        <div className="relative mt-20 flex h-full flex-col items-center">
+        <div className="relative mt-40 flex h-full flex-col items-center">
           <div className="top-50% left-50% -translate-x-50% -translate-y-50%  mb-9">
             <Brand theme={theme === "dark" ? "dark" : "light"} />
           </div>
@@ -23,10 +23,20 @@ export default function ChatPage() {
             <ChatInput />
           </div>
           <div className="w-full">
-            <p className="mb-5 text-center">Recents</p>
-            <div className="grid grid-cols-2 gap-4 sm:grid-cols-4">
-              <div className="border-1 rounded-lg border-gray-300 p-2">01</div>
-              <div>09</div>
+            <p className="text-semibold mb-5 text-lg">Recent Meals</p>
+            <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
+              <div className="border-1 rounded-lg border-gray-300 bg-white p-2 text-black">
+                01
+              </div>
+              <div className="border-1 rounded-lg border-gray-300 bg-white p-2 py-10 text-black">
+                02
+              </div>
+              <div className="border-1 rounded-lg border-gray-300 bg-white p-2 text-black">
+                03
+              </div>
+              <div className="border-1 rounded-lg border-gray-300 bg-white p-2 text-black">
+                04
+              </div>
             </div>
           </div>
         </div>
