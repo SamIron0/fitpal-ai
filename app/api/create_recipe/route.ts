fbb4c24861cd9a828a53f4f72d0349bf5c798c59
SamIron0 <samuelironkwec@gmail.com>
2024-04-22 11:57:54 -0500

    x

--- app/api/create_recipe/route.ts
+++ app/api/create_recipe/route.ts
@@ -1,8 +1,3 @@
-import { oneLine, stripIndent } from "common-tags"
-import { retrieveEmbedding } from "@/components/sidebar/settings/embeddings"
-import { Database, Tables } from "@/supabase/types"
-import { ChatSettings } from "@/types"
-import { OpenAIStream, StreamingTextResponse } from "ai"
 import { ServerRuntime } from "next"
 import { createRecipe } from "@/db/recipes"
 export const runtime: ServerRuntime = "edge"
