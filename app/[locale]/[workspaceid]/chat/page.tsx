fcfc3a40b21e862aa1597f52e00bf98d6fbf9ecb
SamIron0 <samuelironkwec@gmail.com>
2024-04-22 17:16:55 -0500

    x

--- app/[locale]/[workspaceid]/chat/page.tsx
+++ app/[locale]/[workspaceid]/chat/page.tsx
@@ -22,20 +22,35 @@ export default function ChatPage() {
           <div className="w-full max-w-md items-end  pb-3 pt-0  sm:pb-8 sm:pt-5">
             <ChatInput />
           </div>
-          <div className="w-full pt-24">
-            <p className="mb-5 text-xl font-semibold">Recent Meals</p>
-            <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
-              <div className="border-1 rounded-lg border-gray-300 bg-white p-2 text-black">
+          <div className="w-full max-w-4xl pt-24">
+            <p className="mb-5 text-3xl font-semibold">Recent Meals</p>
+            <div className="max-w- grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
+              <div className="border-1 rounded-lg border-gray-300 bg-white p-2 py-10 text-black">
                 01
               </div>
               <div className="border-1 rounded-lg border-gray-300 bg-white p-2 py-10 text-black">
                 02
               </div>
-              <div className="border-1 rounded-lg border-gray-300 bg-white p-2 text-black">
+              <div className="border-1 rounded-lg border-gray-300 bg-white p-2 py-10 text-black">
                 03
               </div>
-              <div className="border-1 rounded-lg border-gray-300 bg-white p-2 text-black">
+              <div className="border-1 rounded-lg border-gray-300 bg-white p-2 py-10 text-black">
                 04
+              </div>
+              <div className="border-1 rounded-lg border-gray-300 bg-white p-2 py-10 text-black">
+                05
+              </div>
+
+              <div className="border-1 rounded-lg border-gray-300 bg-white p-2 py-10 text-black">
+                06
+              </div>
+
+              <div className="border-1 rounded-lg border-gray-300 bg-white p-2 py-10 text-black">
+                07
+              </div>
+
+              <div className="border-1 rounded-lg border-gray-300 bg-white p-2 py-10 text-black">
+                08
               </div>
             </div>
           </div>
