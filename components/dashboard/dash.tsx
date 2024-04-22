42a7e299909b19dfd8451e98833476b811fdb9f5
SamIron0 <samuelironkwec@gmail.com>
2024-04-22 09:49:13 -0500

    z

--- components/dashboard/dash.tsx
+++ components/dashboard/dash.tsx
@@ -3,14 +3,26 @@
 import { useState } from "react"
 import { Input } from "../ui/input"
 import { Button } from "../ui/button"
-
+import axios from "axios"
 interface Props {
   onScrapeUrl: (url: string) => void
 }
 export default function Dash() {
   const [url, setURL] = useState("") // add this line
   const onScrapeUrl = async (url: string) => {
-    console.log(url)
+    var endpoint =
+      "https://3x077l0rol.execute-api.us-east-1.amazonaws.com/main/create-mealplan"
+
+    try {
+      var result = await axios.post(endpoint, {
+        url: url
+      })
+      const res = result.data
+      var data = JSON.parse(res)
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
   }
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center">
