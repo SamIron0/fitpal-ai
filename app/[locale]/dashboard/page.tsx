892f82afa70a5577f9a9c2686dfdcdac474ae6d1
SamIron0 <samuelironkwec@gmail.com>
2024-04-22 07:19:10 -0500

    x

--- app/[locale]/dashboard/page.tsx
+++ app/[locale]/dashboard/page.tsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/ui/button"
 
 export default async function Dashboard() {
   //const { subscription } = useContext(ChatbotUIContext)
+  const supabase = createClient()
 
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center"></div>
