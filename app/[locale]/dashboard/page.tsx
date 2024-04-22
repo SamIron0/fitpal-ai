e19cf8c306eba6986569d6c98fc3204e0978d115
SamIron0 <samuelironkwec@gmail.com>
2024-04-22 07:20:20 -0500

    x

--- app/[locale]/dashboard/page.tsx
+++ app/[locale]/dashboard/page.tsx
@@ -7,8 +7,21 @@ import { Button } from "@/components/ui/button"
 export default async function Dashboard() {
   //const { subscription } = useContext(ChatbotUIContext)
   const supabase = createClient()
+  const {
+    data: { user }
+  } = await supabase.auth.getUser()
 
+  //const [url, setURL] = useState("")
+  const scrapeUrl = async () => {
+    // const res = await fetch(`/api/scrape?url=${url}`)
+    //const data = await res.json()
+    console.log("url")
+  }
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center"></div>
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      {" "}
+      <Input placeholder="Enter URL" onChange={() => {}} />
+      <Button onClick={scrapeUrl}>Submit</Button>
+    </div>
   )
 }
