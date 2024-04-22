67d97dcad67e688905b95dd48fcb3ce460d5e82a
SamIron0 <samuelironkwec@gmail.com>
2024-04-22 18:07:02 -0500

    x

--- app/[locale]/[workspaceid]/chat/page.tsx
+++ app/[locale]/[workspaceid]/chat/page.tsx
@@ -12,42 +12,36 @@ export default function ChatPage() {
   const { theme } = useTheme()
 
   return (
-    <>
-      {chatMessages.length === 0 ? (
-        <div className="relative mt-32  flex h-full flex-col items-center px-4 sm:px-6">
-          <div className="top-50% left-50% -translate-x-50% -translate-y-50%  mb-9">
-            <Brand theme={theme === "dark" ? "dark" : "light"} />
-          </div>
+    <div className="relative mt-32  flex h-full flex-col items-center px-4 sm:px-6">
+      <div className="top-50% left-50% -translate-x-50% -translate-y-50%  mb-9">
+        <Brand theme={theme === "dark" ? "dark" : "light"} />
+      </div>
 
-          <div className="w-full max-w-md items-end  pb-3 pt-0  sm:pb-8 sm:pt-5">
-            <ChatInput />
-          </div>
-          {isGenerating ? (
-            <div
-              role="status"
-              className="grid max-w-4xl animate-pulse grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4"
-            >
-              <div className="border-1 rounded-lg border-gray-300 bg-gray-700 p-2 py-10 text-black"></div>
-              <div className="border-1 rounded-lg border-gray-300 bg-gray-700 p-2 py-10 text-black"></div>
-              <div className="border-1 rounded-lg border-gray-300 bg-gray-700 p-2 py-10 text-black"></div>
-              <div className="border-1 rounded-lg border-gray-300 bg-gray-700 p-2 py-10 text-black"></div>
-              <div className="border-1 rounded-lg border-gray-300 bg-gray-700 p-2 py-10 text-black"></div>
+      <div className="w-full max-w-md items-end  pb-3 pt-0  sm:pb-8 sm:pt-5">
+        <ChatInput />
+      </div>
+      {isGenerating ? (
+        <div
+          role="status"
+          className="grid max-w-4xl animate-pulse grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4"
+        >
+          <div className="border-1 rounded-lg border-gray-300 bg-gray-700 p-2 py-10 text-black"></div>
+          <div className="border-1 rounded-lg border-gray-300 bg-gray-700 p-2 py-10 text-black"></div>
+          <div className="border-1 rounded-lg border-gray-300 bg-gray-700 p-2 py-10 text-black"></div>
+          <div className="border-1 rounded-lg border-gray-300 bg-gray-700 p-2 py-10 text-black"></div>
+          <div className="border-1 rounded-lg border-gray-300 bg-gray-700 p-2 py-10 text-black"></div>
 
-              <div className="border-1 rounded-lg border-gray-300 bg-gray-700 p-2 py-10 text-black"></div>
+          <div className="border-1 rounded-lg border-gray-300 bg-gray-700 p-2 py-10 text-black"></div>
 
-              <div className="border-1 rounded-lg border-gray-300 bg-gray-700 p-2 py-10 text-black"></div>
+          <div className="border-1 rounded-lg border-gray-300 bg-gray-700 p-2 py-10 text-black"></div>
 
-              <div className="border-1 rounded-lg border-gray-300 bg-gray-700 p-2 py-10 text-black"></div>
-            </div>
-          ) : (
-            <div className="w-full max-w-4xl pt-24">
-              <p className="mb-5 text-3xl font-semibold">Recent Meals</p>
-            </div>
-          )}
+          <div className="border-1 rounded-lg border-gray-300 bg-gray-700 p-2 py-10 text-black"></div>
         </div>
       ) : (
-        <ChatUI />
+        <div className="w-full max-w-4xl pt-24">
+          <p className="mb-5 text-3xl font-semibold">Recent Meals</p>
+        </div>
       )}
-    </>
+    </div>
   )
 }
