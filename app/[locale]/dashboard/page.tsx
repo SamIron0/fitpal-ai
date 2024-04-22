261977ffda62328ae175fe2c225470d943f82ce8
SamIron0 <samuelironkwec@gmail.com>
2024-04-22 07:18:38 -0500

    x

---
+++ app/[locale]/dashboard/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import { createClient } from "@/lib/supabase/client"
+import { useState } from "react"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+export default async function Dashboard() {
+  //const { subscription } = useContext(ChatbotUIContext)
+
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center"></div>
+  )
+}
