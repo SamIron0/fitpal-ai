0376e87c867955360d9118a34c93bead8f0ac0db
SamIron0 <samuelironkwec@gmail.com>
2024-04-23 17:48:07 -0500

    z

--- app/[locale]/[workspaceid]/chat/page.tsx
+++ app/[locale]/[workspaceid]/chat/page.tsx
@@ -11,10 +11,10 @@ import { useContext } from "react"
 export default async function ChatPage() {
   const { generatedRecipes, isGenerating } = useContext(ChatbotUIContext)
   const { theme } = useTheme()
-  const recentRecipes = await fetch("api/recipe/get_recipes", {
+  /*const recentRecipes = await fetch("api/recipe/get_recipes", {
     method: "POST",
     body: JSON.stringify({ input: "" })
-  })
+  })*/
   //const res: TablesInsert<"recipes">[] = await recipes.json()
   //console.log(recentRecipes)
 
