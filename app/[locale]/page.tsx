09ebfc5676cc3bd084928dd94b209601c0731670
SamIron0 <samuelironkwec@gmail.com>
2024-04-22 19:30:56 -0500

    z

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,71 +1,11 @@
 "use client"
 
-import { Sidebar } from "@/components/sidebar/sidebar"
-import { SidebarSwitcher } from "@/components/sidebar/sidebar-switcher"
-import { Button } from "@/components/ui/button"
-import { Tabs } from "@/components/ui/tabs"
-import useHotkey from "@/lib/hooks/use-hotkey"
-import { cn } from "@/lib/utils"
-import { ContentType } from "@/types"
-import { IconChevronCompactRight } from "@tabler/icons-react"
-import { usePathname, useRouter, useSearchParams } from "next/navigation"
-import { FC, useState } from "react"
-import { useSelectFileHandler } from "@/components/chat/chat-hooks/use-select-file-handler"
-import { CommandK } from "@/components/utility/command-k"
-import Dashboard from "./dashboard/page"
 import { Brand } from "@/components/ui/brand"
 import { ChatInput } from "@/components/chat/chat-input"
 import { useTheme } from "next-themes"
 
 export default function HomePage() {
-  useHotkey("s", () => setShowSidebar(prevState => !prevState))
-  const SIDEBAR_WIDTH = 350
-  const pathname = usePathname()
-  const router = useRouter()
-  const searchParams = useSearchParams()
-  const tabValue = searchParams.get("tab") || "chats"
-
-  const { handleSelectDeviceFile } = useSelectFileHandler()
-
-  const [contentType, setContentType] = useState<ContentType>(
-    tabValue as ContentType
-  )
-  const [showSidebar, setShowSidebar] = useState(
-    localStorage.getItem("showSidebar") === "true"
-  )
-  const [isDragging, setIsDragging] = useState(false)
-
-  const onFileDrop = (event: React.DragEvent<HTMLDivElement>) => {
-    event.preventDefault()
-
-    const files = event.dataTransfer.files
-    const file = files[0]
-
-    handleSelectDeviceFile(file)
-
-    setIsDragging(false)
-  }
-
-  const handleDragEnter = (event: React.DragEvent<HTMLDivElement>) => {
-    event.preventDefault()
-    setIsDragging(true)
-  }
-
-  const handleDragLeave = (event: React.DragEvent<HTMLDivElement>) => {
-    event.preventDefault()
-    setIsDragging(false)
-  }
-
-  const onDragOver = (event: React.DragEvent<HTMLDivElement>) => {
-    event.preventDefault()
-  }
-
-  const handleToggleSidebar = () => {
-    setShowSidebar(prevState => !prevState)
-    localStorage.setItem("showSidebar", String(!showSidebar))
-  }
   const { theme } = useTheme()
-
   return (
     <div className="relative mt-32  flex h-full flex-col items-center px-4 sm:px-6">
       <div className="top-50% left-50% -translate-x-50% -translate-y-50%  mb-9">
