c29bf3bef1e21f6fa7f7f6f7d06b7f68f6dc126e
SamIron0 <samuelironkwec@gmail.com>
2024-04-24 18:44:08 -0500

    x

--- components/adminDashboard/dash.tsx
+++ components/adminDashboard/dash.tsx
@@ -1,17 +1,32 @@
 "use client"
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Input } from "../ui/input"
 import { Button } from "../ui/button"
 import axios from "axios"
 import { TablesInsert } from "@/supabase/types"
 import { v4 as uuidv4 } from "uuid"
 import { toast } from "sonner"
+import { createClient } from "@/lib/supabase/client"
+import { useRouter } from "next/navigation"
 interface Props {
   onScrapeUrl: (url: string) => void
 }
 export default function Dash() {
   const [url, setURL] = useState("") // add this line
+  const [createdRecipe, setCreatedRecipe] =
+    useState<TablesInsert<"recipes"> | null>({} as any)
+
+  const supabase = createClient()
+  const router = useRouter()
+  useEffect(() => {
+    ;(async () => {
+      const session = (await supabase.auth.getSession()).data.session
+      if (!session) {
+        router.push("/login")
+      }
+    })()
+  }, [])
   const onScrapeUrl = async (url: string) => {
     const toastId = toast.loading("Scraping")
 
@@ -38,21 +53,24 @@ export default function Dash() {
         url: url
       }
       toast.dismiss(toastId)
-      const toastId2 = toast.loading("Creating Recipe")
+      const toastId2 = toast.loading("Saving Recipe")
       // call api to create recipe
-      var createRecipe = await fetch("api/recipe/create_recipe", {
+      var response = await fetch("api/recipe/create_recipe", {
         method: "POST",
         body: JSON.stringify({ recipe: recipe, tags: data.tags })
       })
+
       toast.dismiss(toastId2)
 
-      toast.success("Recipe Created!")
+      toast.success("Saving Created!")
+
+      setCreatedRecipe(recipe)
     } catch (error) {
       console.log(error)
     }
   }
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center">
+    <div className="flex h-screen w-full flex-col items-center justify-center p-4">
       {" "}
       <Input
         value={url}
@@ -61,6 +79,21 @@ export default function Dash() {
         style={{ fontSize: "16px" }}
       />
       <Button onClick={() => onScrapeUrl(url)}>Submit</Button>
+      {createdRecipe ? (
+        <div className="mt-8 flex w-full  max-w-3xl flex-col items-center justify-center border-2 p-2">
+          <div>{createdRecipe.name}</div>
+          <div>{createdRecipe.description}</div>
+          <div>{createdRecipe.ingredients}</div>
+          <div>{createdRecipe.imgurl}</div>
+          <div>{createdRecipe.protein}</div>
+          <div>{createdRecipe.fats}</div>
+          <div>{createdRecipe.carbs}</div>
+          <div>{createdRecipe.calories}</div>
+          <div>{createdRecipe.instructions}</div>
+          <div>{createdRecipe.portions}</div>
+          <div>{createdRecipe.cooking_time}</div>
+        </div>
+      ) : null}{" "}
     </div>
   )
 }
