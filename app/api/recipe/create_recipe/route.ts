a9f35e98c82f6aebc079e18ac9713aadb748e588
SamIron0 <samuelironkwec@gmail.com>
2024-04-25 17:15:18 -0500

    x

--- app/api/recipe/create_recipe/route.ts
+++ app/api/recipe/create_recipe/route.ts
@@ -1,15 +1,19 @@
 import { ServerRuntime } from "next"
-import { createRecipe } from "@/db/recipes"
+import { createRecipe, urlExists } from "@/db/recipes"
 import { TablesInsert } from "@/supabase/types"
 export const runtime: ServerRuntime = "edge"
 
 export async function POST(request: Request) {
   const json = await request.json()
-  const { recipe, tags } = json as {
+  const { recipe, tags, url } = json as {
     recipe: TablesInsert<"recipes">
     tags: string[]
+    url: string
   }
-  //const recipeJson = JSON.stringify(recipe)
+  const exists: boolean = await urlExists(url)
+  if (exists) {
+    return new Response(JSON.stringify({ error: "url already exists" }))
+  }
   try {
     const res = await createRecipe(recipe, tags)
     return new Response(JSON.stringify(res))
--- components/adminDashboard/dash.tsx
+++ components/adminDashboard/dash.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { useEffect, useState } from "react"
 import { Input } from "../ui/input"
 import { Button } from "../ui/button"
@@ -10,12 +8,75 @@ import { toast } from "sonner"
 import { createClient } from "@/lib/supabase/client"
 import { useRouter } from "next/navigation"
 import { urlExists } from "@/db/recipes"
-interface Props {
-  recipe?: TablesInsert<"recipes">
-  handleScrapeUrl: (url: string) => void
-}
-export default function Dash({ recipe, handleScrapeUrl }: Props) {
+
+interface Props {}
+
+export default function Dash() {
+  const supabase = createClient()
+  const router = useRouter()
+  const [recipe, setRecipe] = useState({} as TablesInsert<"recipes">)
   const [url, setUrl] = useState("")
+
+  useEffect(() => {
+    async function checkUser() {
+      const {
+        data: { user }
+      } = await supabase.auth.getUser()
+      const session = (await supabase.auth.getSession()).data.session
+
+      if (session?.user.email !== "ekaronke@gmail.com") {
+        console.log("session", session?.user.email)
+        router.push("/")
+      }
+    }
+
+    checkUser()
+  }, [])
+
+  const handleScrapeUrl = async (url: string) => {
+    if (!url) {
+      toast.error("Please enter a valid URL")
+      return
+    }
+    try {
+      const toastId = toast.loading("Scraping...")
+      const endpoint =
+        "https://7a38-2604-3d09-aa7a-95e0-9ddf-5d76-dfb9-fa06.ngrok-free.app/scrape"
+      const response = await axios.post(endpoint, { url })
+      const data = response.data.body
+      const recipe: TablesInsert<"recipes"> = {
+        id: uuidv4(),
+        name: data.name,
+        description: data.description,
+        ingredients: data.ingredients,
+        imgurl: data.imgurl,
+        protein: data.protein,
+        fats: data.fats,
+        carbs: data.carbs,
+        calories: data.calories,
+        instructions: data.instructions,
+        portions: data.portions,
+        cooking_time: data.cooking_time,
+        url: url
+      }
+
+      const createRecipeResponse = await fetch("api/recipe/create_recipe", {
+        method: "POST",
+        body: JSON.stringify({ recipe: recipe, tags: data.tags, url: url }),
+        headers: { "Content-Type": "application/json" }
+      })
+
+      if (createRecipeResponse.status === 200) {
+        toast.success("Recipe scraped successfully")
+      } else {
+        toast.error("Error scraping recipe")
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error("Error scraping recipe in catch")
+    }
+  }
+
   return (
     <div className="my-12 flex w-full flex-col items-center p-4">
       <div className="flex w-full flex-1 flex-col justify-center gap-2 text-foreground animate-in">
@@ -36,18 +97,16 @@ export default function Dash({ recipe, handleScrapeUrl }: Props) {
           <div>Name:{recipe.name}</div>
           <div>Description: {recipe.description}</div>
           <ul>
-            {" "}
             Ingredients:
             {recipe.ingredients?.map((ingredient, index) => (
               <li key={index}>{ingredient}</li>
-            ))}{" "}
+            ))}
           </ul>
           <div> protein:{recipe.protein}</div>
           <div> fats:{recipe.fats}</div>
           <div> carbs:{recipe.carbs}</div>
           <div> calories:{recipe.calories}</div>
           <ul>
-            {" "}
             instructions:
             {recipe.instructions?.map((instruction, index) => (
               <li key={index}>{instruction}</li>
@@ -56,7 +115,7 @@ export default function Dash({ recipe, handleScrapeUrl }: Props) {
           <div> portions:{recipe.portions}</div>
           <div> cooking_time:{recipe.cooking_time}</div>
         </div>
-      ) : null}{" "}
+      ) : null}
     </div>
   )
 }
