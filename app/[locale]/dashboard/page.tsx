d34e61af6a2374e0282da43853734400861c4931
SamIron0 <samuelironkwec@gmail.com>
2024-04-25 16:04:38 -0500

    x

--- app/[locale]/dashboard/page.tsx
+++ app/[locale]/dashboard/page.tsx
@@ -2,6 +2,12 @@ import { createClient } from "@/lib/supabase/client"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import Dash from "@/components/adminDashboard/dash"
+import { toast } from "sonner"
+import { redirect } from "next/navigation"
+import { urlExists } from "@/db/recipes"
+import axios from "axios"
+import { TablesInsert } from "@/supabase/types"
+import { v4 as uuidv4 } from "uuid"
 
 export default async function Dashboard() {
   //const { subscription } = useContext(ChatbotUIContext)
@@ -9,14 +15,72 @@ export default async function Dashboard() {
   const {
     data: { user }
   } = await supabase.auth.getUser()
-  /*if (user?.id !== "cedf4e14-0575-466b-83bf-ce744196e023") {
-    return
-  }*/
-  //const [url, setURL] = useState("")
 
+  const session = (await supabase.auth.getSession()).data.session
+  if (session?.user.email !== "ekaronke@gmail.com") {
+    return redirect("/")
+  }
+
+  const onScrapeUrl = async (formData: FormData) => {
+    const url = formData.get("url") as string
+    const status = await urlExists(url)
+    if (status) {
+      toast.error("Invalid exists")
+      return
+    }
+    const toastId = toast.loading("Scraping")
+
+    var endpoint =
+      " https://7a38-2604-3d09-aa7a-95e0-9ddf-5d76-dfb9-fa06.ngrok-free.app/scrape"
+
+    try {
+      var result = await axios.post(endpoint, {
+        url: url
+      })
+      const data = result.data.body
+      const recipe: TablesInsert<"recipes"> = {
+        id: uuidv4(),
+        name: data.name,
+        description: data.description,
+        ingredients: data.ingredients,
+        imgurl: data.imgurl,
+        protein: data.protein,
+        fats: data.fats,
+        carbs: data.carbs,
+        calories: data.calories,
+        instructions: data.instructions,
+        portions: data.portions,
+        cooking_time: data.cooking_time,
+        url: url
+      }
+      toast.dismiss(toastId)
+      const toastId2 = toast.loading("Saving Recipe")
+      // call api to create recipe
+      var response = await fetch("api/recipe/create_recipe", {
+        method: "POST",
+        body: JSON.stringify({ recipe: recipe, tags: data.tags })
+      })
+
+      toast.dismiss(toastId2)
+
+      toast.success("Saving Created!")
+      return <Dash recipe={recipe}></Dash>
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
-    <div className="flex">
-      <Dash />
-    </div>
+    <form
+      className="flex w-full flex-1 flex-col justify-center gap-2 text-foreground animate-in"
+      action={onScrapeUrl}
+    >
+      <div className="my-12 flex w-full flex-col items-center p-4">
+        <Input name="url" placeholder={"url"} style={{ fontSize: "16px" }} />
+        <Button formAction={onScrapeUrl} className="mt-6">
+          Scrape
+        </Button>
+        <Dash />
+      </div>
+    </form>
   )
 }
--- components/adminDashboard/dash.tsx
+++ components/adminDashboard/dash.tsx
@@ -11,107 +11,36 @@ import { createClient } from "@/lib/supabase/client"
 import { useRouter } from "next/navigation"
 import { urlExists } from "@/db/recipes"
 interface Props {
-  onScrapeUrl: (url: string) => void
+  recipe?: TablesInsert<"recipes">
 }
-export default function Dash() {
-  const [url, setURL] = useState("") // add this line
-  const [createdRecipe, setCreatedRecipe] =
-    useState<TablesInsert<"recipes"> | null>({} as any)
-
-  const supabase = createClient()
-  const router = useRouter()
-  useEffect(() => {
-    ;(async () => {
-      const session = (await supabase.auth.getSession()).data.session
-      if (session?.user.email !== "ekaronke@gmail.com") {
-        router.push("/")
-      }
-    })()
-  }, [])
-  const onScrapeUrl = async (url: string) => {
-    const status = await urlExists(url)
-    if (!status) {
-      toast.error("Invalid URL")
-      return
-    }
-    const toastId = toast.loading("Scraping")
-
-    var endpoint =
-      " https://7a38-2604-3d09-aa7a-95e0-9ddf-5d76-dfb9-fa06.ngrok-free.app/scrape"
-
-    try {
-      var result = await axios.post(endpoint, {
-        url: url
-      })
-      const data = result.data.body
-      const recipe: TablesInsert<"recipes"> = {
-        id: uuidv4(),
-        name: data.name,
-        description: data.description,
-        ingredients: data.ingredients,
-        imgurl: data.imgurl,
-        protein: data.protein,
-        fats: data.fats,
-        carbs: data.carbs,
-        calories: data.calories,
-        instructions: data.instructions,
-        portions: data.portions,
-        cooking_time: data.cooking_time,
-        url: url
-      }
-      toast.dismiss(toastId)
-      const toastId2 = toast.loading("Saving Recipe")
-      // call api to create recipe
-      var response = await fetch("api/recipe/create_recipe", {
-        method: "POST",
-        body: JSON.stringify({ recipe: recipe, tags: data.tags })
-      })
-
-      toast.dismiss(toastId2)
-
-      toast.success("Saving Created!")
-
-      setCreatedRecipe(recipe)
-    } catch (error) {
-      console.log(error)
-    }
-  }
+export default function Dash({ recipe }: Props) {
   return (
     <div className="my-12 flex w-full flex-col items-center p-4">
       {" "}
-      <Input
-        value={url}
-        onChange={e => setURL(e.target.value)}
-        placeholder={"url"}
-        style={{ fontSize: "16px" }}
-      />
-      <Button className="mt-4 px-10" onClick={() => onScrapeUrl(url)}>
-        Submit
-      </Button>
-      {createdRecipe ? (
+      {recipe ? (
         <div className="mt-8 flex w-full  max-w-3xl flex-col justify-center border-2 p-2">
-          <div>Name:{createdRecipe.name}</div>
-          <div>Description: {createdRecipe.description}</div>
+          <div>Name:{recipe.name}</div>
+          <div>Description: {recipe.description}</div>
           <ul>
             {" "}
             Ingredients:
-            {createdRecipe.ingredients?.map((ingredient, index) => (
+            {recipe.ingredients?.map((ingredient, index) => (
               <li key={index}>{ingredient}</li>
             ))}{" "}
           </ul>
-          <div> protein:{createdRecipe.protein}</div>
-          <div> fats:{createdRecipe.fats}</div>
-          <div> carbs:{createdRecipe.carbs}</div>
-          <div> calories:{createdRecipe.calories}</div>
+          <div> protein:{recipe.protein}</div>
+          <div> fats:{recipe.fats}</div>
+          <div> carbs:{recipe.carbs}</div>
+          <div> calories:{recipe.calories}</div>
           <ul>
             {" "}
             instructions:
-            {createdRecipe.instructions?.map((instruction, index) => (
+            {recipe.instructions?.map((instruction, index) => (
               <li key={index}>{instruction}</li>
             ))}
           </ul>
-          <div> portions:{createdRecipe.portions}</div>
-          <div> cooking_time:{createdRecipe.cooking_time}</div>
+          <div> portions:{recipe.portions}</div>
+          <div> cooking_time:{recipe.cooking_time}</div>
         </div>
       ) : null}{" "}
     </div>
