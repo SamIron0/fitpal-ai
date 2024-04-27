2d0b3691d1f1259477a4cf9f619063109a8efc6c
SamIron0 <samuelironkwec@gmail.com>
2024-04-26 19:47:18 -0500

    s

--- components/meal/meal-drawer.tsx
+++ components/meal/meal-drawer.tsx
@@ -17,7 +17,7 @@ import { Label } from "../ui/label"
 import { Input } from "../ui/input"
 import { SubmitButton } from "../ui/submit-button"
 import { useRouter } from "next/navigation"
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { getCompleteRecipeById } from "@/db/recipes"
 import { ChatbotUIContext } from "@/context/context"
 import { TablesInsert } from "@/supabase/types"
@@ -29,7 +29,8 @@ interface MealDrawerProps {
 export const MealDrawer = ({ children, recipe }: MealDrawerProps) => {
   //get full recipe
   const { generatedRecipes, setGeneratedRecipes } = useContext(ChatbotUIContext)
-
+  const [updatedRecipe, setUpdatedRecipe] =
+    useState<TablesInsert<"recipes">>(recipe)
   useEffect(() => {
     const getCompleteRecipe = async () => {
       const completeRecipe: TablesInsert<"recipes"> =
@@ -38,6 +39,7 @@ export const MealDrawer = ({ children, recipe }: MealDrawerProps) => {
         r.id === recipe.id ? completeRecipe : r
       )
       setGeneratedRecipes(updatedGeneratedRecipes as any)
+      setUpdatedRecipe(completeRecipe)
     }
     getCompleteRecipe()
   }, [])
@@ -53,38 +55,44 @@ export const MealDrawer = ({ children, recipe }: MealDrawerProps) => {
           <div className="mt-8 flex w-full flex-col p-6 ">
             <div className="w-full justify-end">
               <img
-                src={"/images/" + recipe.imgurl}
+                src={"/images/" + updatedRecipe.imgurl}
                 className="mb-2 w-1/2 rounded-lg object-cover"
-                alt={recipe.name || "Recipe Image"}
+                alt={updatedRecipe.name || "updatedRecipe Image"}
               />
             </div>
             <div className="w-full">
-              <p className="pb-3 text-3xl font-semibold">{recipe.name}</p>
-              <p className="text-md">{recipe.description}</p>
+              <p className="pb-3 text-3xl font-semibold">
+                {updatedRecipe.name}
+              </p>
+              <p className="text-md">{updatedRecipe.description}</p>
             </div>
             <div className="w-full">
               <p className="text-2xl font-semibold">Ingredients</p>
               <div className="w-full">
-                {recipe?.ingredients?.map((ingredient: any, index: number) => (
-                  <div
-                    key={index}
-                    className="text-md flex w-full items-center justify-between"
-                  >
-                    <p>•{ingredient}</p>
-                  </div>
-                ))}
+                {updatedRecipe?.ingredients?.map(
+                  (ingredient: any, index: number) => (
+                    <div
+                      key={index}
+                      className="text-md flex w-full items-center justify-between"
+                    >
+                      <p>•{ingredient}</p>
+                    </div>
+                  )
+                )}
               </div>
 
               <h2 className="pt-3 text-2xl font-semibold">Directions</h2>
               <ul className="w-full">
-                {recipe?.instructions?.map((direction: any, index: number) => (
-                  <li
-                    key={index}
-                    className="text-md flex w-full items-center justify-between"
-                  >
-                    •{direction}
-                  </li>
-                ))}
+                {updatedRecipe?.instructions?.map(
+                  (direction: any, index: number) => (
+                    <li
+                      key={index}
+                      className="text-md flex w-full items-center justify-between"
+                    >
+                      •{direction}
+                    </li>
+                  )
+                )}
               </ul>
             </div>
             <div className="flex flex-row items-center pt-3">
@@ -93,13 +101,13 @@ export const MealDrawer = ({ children, recipe }: MealDrawerProps) => {
             </div>
             <div className="w-full space-y-2 pb-6">
               <p className="text-md flex w-full items-center justify-between">
-                Protein: {recipe.protein}g
+                Protein: {updatedRecipe.protein}g
               </p>
               <p className=" text-md flex w-full items-center justify-between">
-                Fat: {recipe.fats}g
+                Fat: {updatedRecipe.fats}g
               </p>
               <p className="text-md flex w-full items-center justify-between">
-                Carbs: {recipe.carbs}g
+                Carbs: {updatedRecipe.carbs}g
               </p>
             </div>
           </div>
