d0a607d105aeb57462a9255dd8956ec7475e9d67
SamIron0 <samuelironkwec@gmail.com>
2024-05-19 14:40:00 -0500

    x

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -58,7 +58,7 @@ export default async function ChatPage() {
                   alt={recipe.name || "Recipe Image"}
                 />
               ) : (
-                <div className="border-1 mb-2 rounded-lg border-gray-300 bg-gray-600 p-2 py-10 text-black"></div>
+                <div className="border-1 mb-2 h-48 rounded-lg border-gray-300 bg-gray-600 p-2 py-10 text-black"></div>
               )}
 
               <p className="text-md w-full text-left">{recipe.name}</p>
