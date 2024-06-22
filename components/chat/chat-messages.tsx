b781e6da77d728217dd1cf5c354f95326924a2fd
SamIron0 <samuelironkwec@gmail.com>
2024-06-22 17:57:27 -0500

    x

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -2,7 +2,6 @@ import { useChatHandler } from "@/components/chat/chat-hooks/use-chat-handler"
 import { ChatbotUIContext } from "@/context/context"
 import { Tables } from "@/supabase/types"
 import { FC, useContext, useState } from "react"
-import { Message } from "../messages/message"
 
 interface ChatMessagesProps {}
 
@@ -22,17 +21,6 @@ export const ChatMessages: FC<ChatMessagesProps> = ({}) => {
           self.findIndex(item => item.id === chatFileItem.id) === _
       )
 
-      return (
-        <Message
-          key={chatMessage.message.sequence_number}
-          message={chatMessage.message}
-          fileItems={messageFileItems}
-          isEditing={editingMessage?.id === chatMessage.message.id}
-          isLast={index === array.length - 1}
-          onStartEdit={setEditingMessage}
-          onCancelEdit={() => setEditingMessage(undefined)}
-          onSubmitEdit={handleSendEdit}
-        />
-      )
+      return <></>
     })
 }
