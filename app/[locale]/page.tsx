3b15903da2c6fd5d20cfb64a17e292ff3bf10000
SamIron0 <samuelironkwec@gmail.com>
2024-06-22 20:06:24 -0500

    x

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { useState, useEffect, useContext } from "react"
 import { ChatInput } from "@/components/chat/chat-input"
 import { LoginDrawer } from "@/components/login/login-drawer"
 import { Brand } from "@/components/ui/brand"
@@ -8,58 +9,73 @@ import { TablesInsert } from "@/supabase/types"
 import axios from "axios"
 import { useTheme } from "next-themes"
 import { useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
 
-export default async function ChatPage() {
+export default function ChatPage() {
   const router = useRouter()
   const { theme } = useTheme()
   const [recipes, setRecipes] = useState<TablesInsert<"recipes">[]>([])
   const supabase = createClient()
 
-  const {
-    data: { session },
-    error
-  } = await supabase.auth.getSession()
-  try {
-    if (session) {
-      const setWs = async () => {
-        const { data: homeWorkspace, error: homeWorkspaceError } =
-          await supabase
-            .from("workspaces")
-            .select("*")
-            .eq("user_id", session?.user.id)
-            .eq("is_home", true)
-            .single()
-
-        if (homeWorkspace) {
-          router.push(`/${homeWorkspace.id}/chat`)
-        } // Do something with the session}
+  useEffect(() => {
+    const fetchSession = async () => {
+      const {
+        data: { session },
+        error
+      } = await supabase.auth.getSession()
+      if (error) {
+        console.error(error)
+        return
       }
-      setWs()
-    }
-    const getForYou = async () => {
-      const data = await fetch("/api/for_you", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json"
-        }
-      })
 
-      const recipes = await data.json()
-      setRecipes(recipes.for_you)
+      if (session) {
+        const setWs = async () => {
+          const { data: homeWorkspace, error: homeWorkspaceError } =
+            await supabase
+              .from("workspaces")
+              .select("*")
+              .eq("user_id", session?.user.id)
+              .eq("is_home", true)
+              .single()
+
+          if (homeWorkspaceError) {
+            console.error(homeWorkspaceError)
+            return
+          }
+
+          if (homeWorkspace) {
+            router.push(`/${homeWorkspace.id}/chat`)
+          }
+        }
+        setWs()
+      }
     }
-    getForYou()
-  } catch (error) {
-    console.error(error)
-  }
+
+    const fetchForYou = async () => {
+      try {
+        const data = await fetch("/api/for_you", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        const recipes = await data.json()
+        setRecipes(recipes.for_you)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    fetchSession()
+    fetchForYou()
+  }, [router, supabase])
 
   return (
-    <div className="hide-scrollbar w-fullrelative flex h-full flex-col items-center overflow-y-auto  px-4 sm:px-6">
-      <div className="top-50% left-50%  -translate-x-50% -translate-y-50% mb-9  mt-32">
+    <div className="hide-scrollbar relative flex size-full flex-col items-center overflow-y-auto px-4 sm:px-6">
+      <div className="top-50% left-50% -translate-x-50% -translate-y-50% mb-9 mt-32">
         <Brand theme={theme === "dark" ? "dark" : "light"} />
       </div>
 
-      <div className="w-full max-w-md items-end  pb-3 pt-0  sm:pb-8 sm:pt-5">
+      <div className="w-full max-w-md items-end pb-3 pt-0 sm:pb-8 sm:pt-5">
         <ChatInput />
       </div>
 
@@ -71,7 +87,7 @@ export default async function ChatPage() {
         >
           {recipes?.map(recipe => (
             <LoginDrawer key={recipe.name}>
-              <div className="flex w-48 flex-col ">
+              <div className="flex w-48 flex-col">
                 {recipe.imgurl ? (
                   <img
                     src={"/images/" + recipe.imgurl}
@@ -81,7 +97,6 @@ export default async function ChatPage() {
                 ) : (
                   <div className="border-1 mb-2 h-48 rounded-lg border-gray-300 bg-gray-600 p-2 py-10 text-black"></div>
                 )}
-
                 <p className="text-md w-full text-left">{recipe.name}</p>
               </div>
             </LoginDrawer>
