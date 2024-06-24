0a085d90582906e1dadcecf5f654c77e28d89d7c
SamIron0 <samuelironkwec@gmail.com>
2024-06-24 17:20:07 -0500

    x

--- context/context.tsx
+++ context/context.tsx
@@ -173,7 +173,7 @@ export const FitpalAIContext = createContext<FitpalAIContext>({
     calories: 0,
     workouts: 0,
     allergies: [],
-    diet: ""
+    diet: "Anything"
   },
   setSettings: () => {},
 
