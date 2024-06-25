af4dd7afd415e47a01143543466480f946aef7f4
SamIron0 <samuelironkwec@gmail.com>
2024-06-24 19:33:51 -0500

    x

--- app/api/recipe/get_recipes/route.ts
+++ app/api/recipe/get_recipes/route.ts
@@ -21,7 +21,7 @@ export async function POST(request: Request) {
         body: JSON.stringify({
           query: input,
           diet: diet,
-          allery: allergy
+          allergy: allergy
         })
       }
     )
