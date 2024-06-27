af1d50c20d2938521666e21ff1803e3fa564c8fc
SamIron0 <samuelironkwec@gmail.com>
2024-06-27 14:27:04 -0500

    x

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -11,10 +11,9 @@ export const uploadImage = async (file: any) => {
   try {
     const formData = new FormData()
     formData.append("file", file)
-    formData.append("upload_preset", "your_upload_preset") // replace with your Cloudinary upload preset
 
     const response = await fetch(
-      `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload`,
+      `https://api.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload`,
       {
         method: "POST",
         body: formData
