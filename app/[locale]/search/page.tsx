4dc66569e2725d37326fd3ac9b2f702620c57f91
SamIron0 <samuelironkwec@gmail.com>
2024-06-26 20:14:56 -0500

    x

--- app/[locale]/search/page.tsx
+++ app/[locale]/search/page.tsx
@@ -79,7 +79,7 @@ export default function SearchPage() {
                     alt={recipe.name || "Recipe Image"}
                   />
                 ) : (
-                  <div className="border-1 mb-2 h-48 rounded-lg border-input bg-zinc-600 p-2 py-10 text-black"></div>
+                  <div className="border-1 mb-2 h-48 rounded-lg border-input bg-input p-2 py-10 text-black"></div>
                 )}
                 <p className="text-md w-full text-left">{recipe.name}</p>
               </div>
