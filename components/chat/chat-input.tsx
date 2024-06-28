f03950d9593d2c02d3acd7c5e732070dcf038f0c
SamIron0 <samuelironkwec@gmail.com>
2024-06-28 16:03:51 -0500

    x

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -1,22 +1,9 @@
 "use client"
 import { FitpalAIContext } from "@/context/context"
-import useHotkey from "@/lib/hooks/use-hotkey"
 import { cn } from "@/lib/utils"
-import {
-  IconBolt,
-  IconCirclePlus,
-  IconPlayerStopFilled,
-  IconSend
-} from "@tabler/icons-react"
-import Image from "next/image"
+import { IconPlayerStopFilled, IconSend } from "@tabler/icons-react"
 import { FC, useContext, useEffect, useRef, useState } from "react"
 import { useTranslation } from "react-i18next"
-import { toast } from "sonner"
-import { TextareaAutosize } from "../ui/textarea-autosize"
-import { ChatCommandInput } from "./chat-command-input"
-import { ChatFilesDisplay } from "./chat-files-display"
-import { useChatHandler } from "./chat-hooks/use-chat-handler"
-import { useRouter } from "next/navigation"
 import { createClient } from "@/lib/supabase/client"
 import { LoginDrawer } from "../login/login-drawer"
 interface ChatInputProps {}
@@ -73,7 +60,6 @@ export const ChatInput: FC<ChatInputProps> = ({}: ChatInputProps) => {
 
   const [isTyping, setIsTyping] = useState<boolean>(false)
   const chatInputRef = useRef<HTMLInputElement>(null)
-  const { handleStopMessage } = useChatHandler()
   const [input, setInput] = useState<string>("")
   const generateMeals = async () => {
     setIsGenerating(true)
@@ -119,14 +105,7 @@ export const ChatInput: FC<ChatInputProps> = ({}: ChatInputProps) => {
   }
   return (
     <>
-      <div className="flex flex-col flex-wrap justify-center gap-2">
-        <ChatFilesDisplay />
-      </div>
-
       <div className="relative mt-3 flex min-h-[60px] w-full items-center justify-center rounded-xl border-2 border-input">
-        <div className="absolute bottom-[76px] left-0 max-h-[300px] w-full overflow-auto rounded-xl dark:border-none">
-          <ChatCommandInput />
-        </div>
         <input
           ref={chatInputRef}
           className="text-md min-w-3xl flex w-full resize-none rounded-md border-none bg-transparent py-2 pl-3 pr-14 ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
@@ -141,7 +120,7 @@ export const ChatInput: FC<ChatInputProps> = ({}: ChatInputProps) => {
           {isGenerating ? (
             <IconPlayerStopFilled
               className="animate-pulse rounded bg-transparent p-1 hover:bg-background"
-              onClick={handleStopMessage}
+              onClick={() => {}}
               size={30}
             />
           ) : session ? (
