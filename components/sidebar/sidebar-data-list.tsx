d8da21b5390003974cb97b169402d24aea229d90
SamIron0 <samuelironkwec@gmail.com>
2024-06-29 10:51:49 -0500

    x

--- components/sidebar/sidebar-data-list.tsx
+++ components/sidebar/sidebar-data-list.tsx
@@ -5,7 +5,6 @@ import { Tables } from "@/supabase/types"
 import { ContentType, DataItemType, DataListType } from "@/types"
 import { updateAssistant } from "@/db/assistants"
 import { updateCollection } from "@/db/collections"
-import { updateFile } from "@/db/files"
 import { updateModel } from "@/db/models"
 import { updatePrompt } from "@/db/prompts"
 import { updateTool } from "@/db/tools"
@@ -47,94 +46,11 @@ export const SidebarDataList: FC<SidebarDataListProps> = ({
 
   const [isDragOver, setIsDragOver] = useState(false)
 
-  const updateFunctions = {
-    chats: updateChat,
-    presets: updatePreset,
-    prompts: updatePrompt,
-    files: updateFile,
-    calculator: updateCalculator,
-    collections: updateCollection,
-    assistants: updateAssistant,
-    tools: updateTool,
-    models: updateModel
-  }
-
-  const stateUpdateFunctions = {
-    chats: setChats,
-    presets: setPresets,
-    prompts: setPrompts,
-    files: setFiles,
-    calculator: setCalculator,
-    collections: setCollections,
-    assistants: setAssistants,
-    tools: setTools,
-    models: setModels
-  }
-  const updateFolder = async (itemId: string, folderId: string | null) => {
-    const item: any = data.find(item => item.id === itemId)
-
-    if (!item) return null
-
-    const updateFunction = updateFunctions[contentType]
-    const setStateFunction = stateUpdateFunctions[contentType]
-
-    if (!updateFunction || !setStateFunction) return
-
-    const updatedItem = await updateFunction(item.id, { folder_id: folderId })
-
-    setStateFunction((items: any) =>
-      items.map((item: any) =>
-        item.id === updatedItem.id ? updatedItem : item
-      )
-    )
-  }
-
-  const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault()
-    setIsDragOver(true)
-  }
-
-  const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault()
-    setIsDragOver(false)
-  }
-
-  const handleDragStart = (e: React.DragEvent<HTMLDivElement>, id: string) => {
-    e.dataTransfer.setData("text/plain", id)
-  }
-
-  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault()
-  }
-
-  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault()
-    const target = e.target as Element
-
-    if (!target.closest("#folder")) {
-      const itemId = e.dataTransfer.getData("text/plain")
-      updateFolder(itemId, null)
-    }
-    setIsDragOver(false)
-  }
-
   return (
     <>
-      <div
-        ref={divRef}
-        className="mt-2 flex flex-col overflow-auto"
-        onDrop={handleDrop}
-      >
+      <div ref={divRef} className="mt-2 flex flex-col overflow-auto">
         {contentType === "presets" && <Settings />}
       </div>
-
-      <div
-        className={cn("flex grow", isDragOver && "bg-accent")}
-        onDrop={handleDrop}
-        onDragEnter={handleDragEnter}
-        onDragLeave={handleDragLeave}
-        onDragOver={handleDragOver}
-      />
     </>
   )
 }
