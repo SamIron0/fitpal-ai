dbb15ba3bb8066a5ed8b7b67a8d9fc5ba785f1af
SamIron0 <samuelironkwec@gmail.com>
2024-06-29 20:51:59 -0500

    x

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,7 +1,6 @@
 import { ServerRuntime } from "next"
 import { cookies } from "next/headers"
 import { createClient } from "@/lib/supabase/server"
-import { redirect } from "next/navigation"
 export const runtime: ServerRuntime = "edge"
 
 export async function POST(request: Request) {
@@ -40,34 +39,7 @@ export async function POST(request: Request) {
       )
     }
 
-    const { data: homeWorkspace, error: homeWorkspaceError } = await supabase
-      .from("workspaces")
-      .select("*")
-      .eq("user_id", data.user.id)
-      .eq("is_home", true)
-      .single()
-
-    if (!homeWorkspace) {
-      return new Response(
-        JSON.stringify({
-          errors: [
-            {
-              status: "404",
-              title: "Home workspace not found",
-              detail: homeWorkspaceError?.message
-            }
-          ]
-        }),
-        {
-          status: 404,
-          headers: {
-            "Content-Type": "application/json"
-          }
-        }
-      )
-    }
-
-    return new Response(JSON.stringify(homeWorkspace))
+    return new Response(JSON.stringify(data))
   } catch (error) {
     return new Response(JSON.stringify({ error: error }))
   }
