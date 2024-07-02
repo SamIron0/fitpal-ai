afb43d6e68181a47b40425c82f5300707af5abe3
SamIron0 <samuelironkwec@gmail.com>
2024-07-01 23:09:04 -0500

    x

--- components/recipe/recipe-card.tsx
+++ components/recipe/recipe-card.tsx
@@ -4,6 +4,15 @@ import { Clock } from "lucide-react"
 interface RecipeCardProps {
   recipe: Tables<"recipes">
 }
+const convertTime = (totalMinutes: number) => {
+  const hours = Math.floor(totalMinutes / 60)
+  const minutes = totalMinutes % 60
+  if (hours > 0) {
+    return `${hours} hr ${minutes} min`
+  } else {
+    return `${minutes} min`
+  }
+}
 export const RecipeCard = ({ recipe }: RecipeCardProps) => {
   return (
     <div className="flex w-48 flex-col">
@@ -19,7 +28,9 @@ export const RecipeCard = ({ recipe }: RecipeCardProps) => {
       <p className="text-md w-full text-left">{recipe.name}</p>
       <div className="flex w-full text-sm font-light items-center text-gray-300">
         <Clock className="w-4 h-4 mr-2" />
-        <p className="  text-left">{recipe.total_time}</p>
+        <p className="  text-left">
+          {convertTime(recipe.total_time as unknown as number)}
+        </p>
       </div>
     </div>
   )
