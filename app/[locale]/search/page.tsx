9bb85d0b8417ed6428995ac7bcd2a9155eecbdf7
SamIron0 <samuelironkwec@gmail.com>
2024-07-02 09:25:19 -0500

    x

--- app/[locale]/search/page.tsx
+++ app/[locale]/search/page.tsx
@@ -1,34 +1,21 @@
-import { useContext, useEffect, useState } from "react"
-import { useRouter } from "next/navigation"
-import { useTheme } from "next-themes"
-import Head from "next/head"
-import { ChatInput } from "@/components/chat/chat-input"
-import { MealDrawer } from "@/components/meal/meal-drawer"
-import { Brand } from "@/components/ui/brand"
-import { FitpalAIContext } from "@/context/context"
-import { getSettingsByUserId } from "@/db/settings"
-import { Tables } from "@/supabase/types"
+import SearchPage from "@/components/search-page"
 import { Dashboard } from "@/components/ui/dashboard"
-import { Clock } from "lucide-react"
+import { getForYou, getGuestForYou } from "@/db/admin"
 import { createClient } from "@/lib/supabase/server"
 import { cookies } from "next/headers"
-import SearchPage from "@/components/search-page"
 
 export default async function Search() {
   const supabase = createClient(cookies())
   const {
     data: { session }
   } = await supabase.auth.getSession()
+  let forYou: Tables<"recipes">[] = []
 
-  const response = await fetch("/api/for_you", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-
-  const data = await response.json()
-  const forYou: Tables<"recipes">[] = data.for_you
+  if (session) {
+    forYou = await getForYou(session?.user.id)
+  } else {
+    forYou = await getGuestForYou()
+  }
 
   return (
     <Dashboard>
