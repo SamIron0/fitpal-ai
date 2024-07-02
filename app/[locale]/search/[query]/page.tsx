6c3cf045ba5136ef6837982f48d7c600231d9138
SamIron0 <samuelironkwec@gmail.com>
2024-07-02 18:13:39 -0500

    x

--- app/[locale]/search/[query]/page.tsx
+++ app/[locale]/search/[query]/page.tsx
@@ -1,3 +1,49 @@
-export default function Page({ params }: { params: { query: string } }) {
-  return <div>My Post: {params.query}</div>
-}
+import { getRecipeById } from "@/db/admin"
+import { getSettingsById } from "@/db/settings"
+import { createClient } from "@/lib/supabase/server"
+import { Tables } from "@/supabase/types"
+import { cookies } from "next/headers"
+
+export default async function ResultPage({ params }: { params: { query: string } }) {
+  console.log('query',params.query)
+  const supabase = createClient(cookies())
+  const session=await supabase.auth.getSession()
+  let settings: Tables<"settings">={} as Tables<"settings">
+  if (session) {
+    let settings = await getSettingsById(session.data.session?.user.id as string)
+  }
+  const herokuPromise = await fetch("https://fitpal-search.onrender.com/search", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      method: "POST",
+      body: JSON.stringify({
+        input: params.query,
+        diet: settings?.diet || ['Anything'],
+        allergy: settings?.allergies || ['None'],
+      })
+    })
+  }).then(data => data.json())
+
+  const [responseData] = await Promise.all([herokuPromise])
+  const recipeIds = responseData.result
+
+  // Fetch recipes in parallel
+  const recipePromises = recipeIds.map((recipeId: string) =>
+    getRecipeById(recipeId)
+  )
+  const recipes = await Promise.all(recipePromises)
+
+  return (
+    <div>
+      hi
+      {recipes.map((recipe: any) => (
+        <div key={recipe.id}>
+          <h1>{recipe.title}</h1>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  )}
