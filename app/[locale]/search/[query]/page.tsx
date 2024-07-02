7cc6b4615a6d177a67baf420618e0cdd8823968d
SamIron0 <samuelironkwec@gmail.com>
2024-07-02 18:07:51 -0500

    x

--- app/[locale]/search/[query]/page.tsx
+++ app/[locale]/search/[query]/page.tsx
@@ -1,10 +1,3 @@
 export default function Page({ params }: { params: { slug: string } }) {
   return <div>My Post: {params.slug}</div>
 }
-export async function generateStaticParams() {
-  //const posts = await fetch('https://.../posts').then((res) => res.json())
- const posts= [ { slug: "1" }, { slug: "2" }, { slug: "3" } ]
-  return posts.map((post: any) => ({
-    query: post.slug,
-  }))
-}
