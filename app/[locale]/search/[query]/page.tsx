739869bfc63985845a8d853b0c0b6b150d3871f8
SamIron0 <samuelironkwec@gmail.com>
2024-07-02 19:19:30 -0500

    x

--- app/[locale]/search/[query]/page.tsx
+++ app/[locale]/search/[query]/page.tsx
@@ -4,31 +4,34 @@ import { createClient } from "@/lib/supabase/server"
 import { Tables } from "@/supabase/types"
 import { cookies } from "next/headers"
 
-export default async function ResultPage({ params }: { params: { query: string } }) {
+export default async function ResultPage({
+  params
+}: {
+  params: { query: string }
+}) {
   //console.log('query',params.query)
   const supabase = createClient(cookies())
-  const session=(await supabase.auth.getSession()).data.session
-  let settings: Tables<"settings">={} as Tables<"settings">
+  const session = (await supabase.auth.getSession()).data.session
+  let settings: Tables<"settings"> = {} as Tables<"settings">
   if (session) {
-    console.log('session',session)
+    console.log("session", session)
     let settings = await getSettingsById(session.user.id)
   }
-  const herokuPromise = await fetch("https://fitpal-search.onrender.com/search", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-  
-    body: JSON.stringify({
-        input: "rice",
-        diet:  "Anything",
-        allergy:["None"],
-          })
-  })
-    console.log('rennd',herokuPromise)
-  return (
-    <div>
-      hi
-     
-    </div>
-  )}
+  const herokuPromise = await fetch(
+    "https://fitpal-search.onrender.com/search",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+
+      body: JSON.stringify({
+        query: "rice",
+        diet: "Anything",
+        allergy: ["None"]
+      })
+    }
+  )
+  console.log("rennd", herokuPromise.json())
+  return <div>hi</div>
+}
