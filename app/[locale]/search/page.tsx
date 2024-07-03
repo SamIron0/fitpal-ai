711403753c029612fb27cdc953551df66875ce30
SamIron0 <samuelironkwec@gmail.com>
2024-07-02 20:46:14 -0500

    x

--- app/[locale]/search/page.tsx
+++ app/[locale]/search/page.tsx
@@ -5,7 +5,6 @@ import { createClient } from "@/lib/supabase/server"
 import { Tables } from "@/supabase/types"
 import Head from "next/head"
 import { cookies } from "next/headers"
-
 export default async function Search() {
   const supabase = createClient(cookies())
   const {
