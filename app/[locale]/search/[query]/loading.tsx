6ad6a243114e2eb37c2e84527b5ca623fc0870e4
SamIron0 <samuelironkwec@gmail.com>
2024-07-09 18:35:02 -0500

    x

--- app/[locale]/search/[query]/loading.tsx
+++ app/[locale]/search/[query]/loading.tsx
@@ -5,7 +5,7 @@ export default function Loading() {
       <div className="grid w-full max-w-4xl gap-4 sm:grid-cols-2 lg:grid-cols-3">
         {[...Array(8)].map((_, index) => (
           <div key={index} className="flex w-full flex-col">
-            <div className="mb-2 h-48 w-64 rounded-lg bg-input animate-pulse"></div>
+            <div className="mb-2 h-48 w-full rounded-lg bg-input animate-pulse"></div>
             <div className="h-6 w-3/4 bg-input animate-pulse rounded mb-2"></div>
             <div className="flex items-center">
               <div className="h-4 w-4 bg-input animate-pulse rounded mr-2"></div>
