4cc18511bbbb10e326b2fa1226e5663c697daf41
SamIron0 <samuelironkwec@gmail.com>
2024-07-09 15:37:49 -0500

    x

--- components/ui/Navbar/index.tsx
+++ components/ui/Navbar/index.tsx
@@ -15,6 +15,7 @@ import { IconHome, IconLogin, IconLogout } from "@tabler/icons-react"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
 import { useContext } from "react"
+import { Button } from "../button"
 
 interface NavbarProps {
   user: User | undefined
@@ -31,23 +32,25 @@ const Navbar = ({ user }: NavbarProps) => {
     <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center  justify-between border-b border-[#232325] bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
       <div className="flex w-full items-center justify-end space-x-2 pr-3 lg:pr-12">
         <DropdownMenu>
-          <DropdownMenuTrigger className="focus:outline-none p-2 border border-input">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="icon icon-tabler icons-tabler-outline icon-tabler-menu"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M4 8l16 0" />
-              <path d="M4 16l16 0" />
-            </svg>
+          <DropdownMenuTrigger className="focus:outline-none">
+            <Button variant="outline">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="icon icon-tabler icons-tabler-outline icon-tabler-menu"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M4 8l16 0" />
+                <path d="M4 16l16 0" />
+              </svg>
+            </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent>
             <DropdownMenuItem onClick={() => router.push("/")}>
@@ -62,7 +65,8 @@ const Navbar = ({ user }: NavbarProps) => {
             ) : (
               <DropdownMenuItem onClick={() => router.push("/login")}>
                 {" "}
-                <IconLogin  className="mr-1" size={20} />Login
+                <IconLogin className="mr-1" size={20} />
+                Login
               </DropdownMenuItem>
             )}
           </DropdownMenuContent>
