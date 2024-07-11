1eb9fc489ea216e9dd70d738469dcfe8e03a956f
SamIron0 <samuelironkwec@gmail.com>
2024-07-11 12:37:51 -0500

    x

--- app/api/recipe/upvote/route.ts
+++ app/api/recipe/upvote/route.ts
@@ -1,11 +1,18 @@
 import { vote } from "@/db/recipes"
+import { createClient } from "@/lib/supabase/server"
 import { ServerRuntime } from "next"
+import { cookies } from "next/headers"
 
 export const runtime: ServerRuntime = "edge"
 
 export async function POST(request: Request) {
   const json = await request.json()
-  const { user_id, recipe_id } = json
+  const { recipe_id } = json
+  const supabase = createClient(cookies())
+  const user_id = (await supabase.auth.getUser()).data.user?.id
+  if (!user_id) {
+    return new Response("Not logged in")
+  }
   await vote(1, user_id, recipe_id)
   return new Response("Upvoted")
 }
